1cd1a5ca345df9ae3e39a7dfbc2fc123d635e7c2
moosool <mhmousavi77@gmail.com>
2022-09-06 02:18:53 +0430

    debug spark

--- spark-submit.sh
+++ spark-submit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./venv/lib/python3.9/site-packages/pyspark/bin/spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 main.py
+spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 main.py
