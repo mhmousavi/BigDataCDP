7c1832a74cf1a4339e6f68d7f70d61ce2d7bb915
moosool <mhmousavi77@gmail.com>
2022-09-07 11:04:31 +0430

    separate container

--- spark-submit.sh
+++ spark-submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 main.py
-spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 main_click.py
+#spark-submit --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 main_click.py
 #spark-submit --master local --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 main.py
