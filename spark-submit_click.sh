3a3d0b1a981d9eb788873fad2d0e19627fe5c8ec
moosool <mhmousavi77@gmail.com>
2022-09-07 19:57:06 +0430

    spark submit conf

--- spark-submit_click.sh
+++ spark-submit_click.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-spark-submit --num-executors 4 --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 clicks.py
+spark-submit --num-executors 4 --executor-cores 2 --master spark://spark-master:7077 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 clicks.py
