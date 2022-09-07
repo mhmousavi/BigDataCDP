1a4a13745910904337b509d7343e4a6d62441cb0
moosool <mhmousavi77@gmail.com>
2022-09-07 13:00:32 +0430

    just company1

--- clicks.py
+++ clicks.py
@@ -5,6 +5,6 @@ import kafka
 from main import read_kafka_df, clicks_per_hour
 
 consumer = kafka.KafkaConsumer(bootstrap_servers=os.getenv("BOOTSRAP_SERVER").split(","))
-for topic in consumer.topics():
-    df = read_kafka_df(topic)
-    clicks_per_hour(df, topic)
+# for topic in consumer.topics():
+df = read_kafka_df('company1')
+clicks_per_hour(df, 'company1')
--- login.py
+++ login.py
@@ -5,6 +5,6 @@ import kafka
 from main import read_kafka_df, current_active_sessions
 
 consumer = kafka.KafkaConsumer(bootstrap_servers=os.getenv("BOOTSRAP_SERVER").split(","))
-for topic in consumer.topics():
-    df = read_kafka_df(topic)
-    current_active_sessions(df, topic)
+# for topic in consumer.topics():
+df = read_kafka_df('company1')
+current_active_sessions(df, 'company1')
